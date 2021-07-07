a12b48358137a18d79249836ed6f9f9fdb83d4b9
tuvrai <tuvrai@wp.pl>
2021-07-07 17:12:39 +0200

    master replacement

---
+++ Wpf_Online_Shop/Wpf_Online_Shop/Model/Converter/ParseModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Online_Shop.Model.Converter
+{
+    static public class ParseModel
+    {
+        public static UserModel getUserModelFromSqliteRecord(SQLiteDataReader r)
+        {
+            int id = Convert.ToInt32(r["Id"]);
+            string login = Convert.ToString(r["Login"]);
+            string email = Convert.ToString(r["Email"]);
+            string desc = Convert.ToString(r["Description"]);
+            string firstname = Convert.ToString(r["Firstname"]);
+            string lastname = Convert.ToString(r["Lastname"]);
+
+            UserModel um = new UserModel();
+            um.Id = id;
+            um.Login = login;
+            um.Email = email;
+            um.Description = desc;
+            um.FirstName = firstname;
+            um.LastName = lastname;
+            um.Password = null;
+            return um;
+        }
+    }
+}
---
+++ Wpf_Online_Shop/Wpf_Online_Shop/ViewModel/ProductsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_Online_Shop.ViewModel
+{
+    using BaseClass;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class ProductsViewModel : ViewModel
+    {
+        public ProductsViewModel()
+        {
+
+        }
+
+    }
+}
