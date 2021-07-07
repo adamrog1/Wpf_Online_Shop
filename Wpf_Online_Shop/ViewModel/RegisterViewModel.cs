a3a6ddbbfc590060c094dd84517383eaa6bfca23
tuvrai <tuvrai@wp.pl>
2021-07-07 14:34:10 +0200

    efgqwef

--- Wpf_Online_Shop/ViewModel/RegisterViewModel.cs
+++ Wpf_Online_Shop/ViewModel/RegisterViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wpf_Online_Shop.Model;
 
 namespace Wpf_Online_Shop.ViewModel
 {
@@ -26,6 +27,53 @@ namespace Wpf_Online_Shop.ViewModel
         {
             get { return passwd; }
             set { passwd = value; }
+        }
+
+        private ICommand registerCom;
+        public ICommand RegisterCom
+        {
+            get
+            {
+                return registerCom ?? (registerCom = new RelayCommand(
+                    (p) => {
+                        UserModel user = new UserModel();
+                        user.Login = "adrian1";
+                        user.Password = "szafagra";
+                        user.Email = "adrian@onet.pl";
+                        user.FirstName = "Adrian";
+                        user.LastName = "Rekin";
+                        try
+                        {
+                            if (Model.DatabaseConnection.SqliteInsert.RegisterUser(user))
+                            {
+                                MessageBox.Show("Zarejestrowano poprawnie!");
+                            }
+                            else
+                            {
+                                MessageBox.Show("Błąd");
+                            }
+                        }
+                        catch(System.Data.SQLite.SQLiteException e)
+                        {
+                            if (e.ErrorCode == 19)
+                            {
+                                MessageBox.Show("Istnieje już użytkownik o podanym adresie mailowym lub/i loginie.");
+                            }
+                            else if (e.ErrorCode == 1)
+                            {
+                                MessageBox.Show("Nie można połączyć się z bazą danych");
+                            }
+                            else
+                            {
+                                MessageBox.Show(e.Message);
+                            }
+                        }
+                    }, p => true));
+            }
+            set
+            {
+
+            }
         }
 
         public RegisterViewModel()
