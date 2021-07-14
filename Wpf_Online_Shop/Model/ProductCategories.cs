40880468b0f3e3a3f52e4771750ebf3a98b0525e
tuvrai <tuvrai@wp.pl>
2021-07-14 13:43:51 +0200

    nowe kategorie produktów

--- Wpf_Online_Shop/Model/ProductCategories.cs
+++ Wpf_Online_Shop/Model/ProductCategories.cs
@@ -10,7 +10,7 @@ namespace Wpf_Online_Shop.Model
     {
         public static string[] Get()
         {
-            return new string[3] { "Owoce i warzywa", "Nabiał", "Pieczywo" };
+            return new string[5] { "Owoce i warzywa", "Nabiał", "Pieczywo","Mięso","Napoje" };
         }
     }
 }
