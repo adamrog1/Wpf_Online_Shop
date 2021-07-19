0498f74d8ab00483a868a399e63dde897848df9f
MikolajSynecki <56276942+MikolajSynecki@users.noreply.github.com>
2021-07-19 19:57:12 +0200

    Zmiany widoku

--- Wpf_Online_Shop/View/ProfileView.xaml.cs
+++ Wpf_Online_Shop/View/ProfileView.xaml.cs
@@ -24,5 +24,10 @@ namespace Wpf_Online_Shop.View
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
